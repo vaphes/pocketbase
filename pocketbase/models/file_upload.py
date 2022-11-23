c8297852ce0b3ae731f1fbb195d6e1c9b2ee6f53
Martin <mahe@quantentunnel.de>
2022-11-23 19:30:47 +0100

    Switch from JSON to multipart file encoding for file upload (#6)
    File upload is detected when body data contains a value of class FileUpload
    Remaining JSON is converted to FormData
    Enitre message is sent as multipart

---
+++ pocketbase/models/file_upload.py
@@ -0,0 +1,13 @@
+from httpx._types import FileTypes
+from typing import Sequence, Union
+FileUploadTypes = Union[FileTypes, Sequence[FileTypes]]
+
+
+class FileUpload:
+    def __init__(self, *args):
+        self.files: FileUploadTypes = args
+
+    def get(self, key: str):
+        if isinstance(self.files[0], Sequence):
+            return tuple((key, i) for i in self.files)
+        return ((key, self.files),)
