c0afb2050354296731a759d7957827792b9ff20d
Dan Sikes <dansikes7@gmail.com>
2023-04-09 12:39:09 -0400

    adding dataclass to record model (#33)
    Co-authored-by: Dan Sikes <dansikes@Dans-MacBook-Pro.local>

--- pocketbase/models/record.py
+++ pocketbase/models/record.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 from pocketbase.models.utils.base_model import BaseModel
 from pocketbase.utils import camel_to_snake
+from dataclasses import dataclass, field
 
-
+@dataclass
 class Record(BaseModel):
     collection_id: str
-    collection_name: str
-    expand: dict
+    collection_name: str = ""
+    expand: dict = field(default_factory=dict)
 
     def load(self, data: dict) -> None:
         super().load(data)
