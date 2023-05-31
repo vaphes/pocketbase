d153a413e458e560da0e594938022a571fa106da
Aziz Berkay Yesilyurt <abyesilyurt@gmail.com>
2023-05-31 14:21:41 +0200

    Revert "adding dataclass to record model (#33)" (#42)
    Record class was broken after this commit. See https://github.com/vaphes/pocketbase/issues/37
    This reverts commit c0afb2050354296731a759d7957827792b9ff20d.

--- pocketbase/models/record.py
+++ pocketbase/models/record.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 
 from pocketbase.models.utils.base_model import BaseModel
 from pocketbase.utils import camel_to_snake
-from dataclasses import dataclass, field
 
-@dataclass
+
 class Record(BaseModel):
     collection_id: str
-    collection_name: str = ""
-    expand: dict = field(default_factory=dict)
+    collection_name: str
+    expand: dict
 
     def load(self, data: dict) -> None:
         super().load(data)
