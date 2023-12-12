1b6046c616a06c4cb7d5a3e74e9dc1eaed38fecb
Martin <mahe@quantentunnel.de>
2023-12-13 00:34:19 +0100

    Rename LogService methods and api endpoint url. Fixes #74

--- tests/integration/test_log_service.py
+++ tests/integration/test_log_service.py
@@ -3,17 +3,17 @@ from pocketbase import PocketBase
 
 class TestLogService:
     def test_get_request_list(self, client: PocketBase, state):
-        state.list = client.logs.get_request_list()
+        state.list = client.logs.get_list()
 
     def test_get_request(self, client: PocketBase, state):
         if len(state.list.items) > 0:
             v = state.list.items[0]
-            r = client.logs.get_request(v.id)
+            r = client.logs.get(v.id)
             assert v.id == r.id
             assert v.created == r.created
 
     def test_log_request_stats(self, client: PocketBase, state):
-        state.stats = client.logs.get_requests_stats()
+        state.stats = client.logs.get_stats()
         assert len(state.stats) > 0
         for v in state.stats:
             assert v.total > 0
